e314f6e823ac140870ad0ab3211757df15205ccc
Hege <dani940414@gmail.com>
2016-10-31 15:18:02 +0100

    input processed

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,14 +4,33 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
-        ArrayList<String[]> inputStringArray;
-        inputStringArray = readingInputFromConsole();
+        ArrayList<String[]> input;
+        input = readingInputFromConsole();
 
-        System.out.print("dfasfds");
+        int I = IFromInput(input);
+        int J = JFromInput(input);
+        int L = LFromInput(input);
+        double Beta = BetaFromInput(input);
+
+        ArrayList<List<Double>> H = heightmapFromInput(input, I, J);
+
+        System.out.println(H);
+
+        /*
+5,6,2,100.0
+0.20,0.50,2.51,1.12,6.98,4.43
+1.08,0.20,1.87,1.31,1.44,3.96
+0.67,4.29,4.48,5.68,6.03,3.49
+3.93,1.64,1.84,3.32,2.08,4.63
+4.89,7.30,3.20,2.38,3.04,0.73
+
+         */
+
     }
 
     public static ArrayList<String[]> readingInputFromConsole() throws IOException {
@@ -34,5 +53,33 @@ public class Main {
             }
         }
         return inputstr;
+    }
+
+    public static int IFromInput(ArrayList<String[]> input) {
+        return Integer.parseInt(input.get(0)[0]);
+    }
+
+    public static int JFromInput(ArrayList<String[]> input) {
+        return Integer.parseInt(input.get(0)[1]);
+    }
+
+    public static int LFromInput(ArrayList<String[]> input) {
+        return Integer.parseInt(input.get(0)[2]);
+    }
+
+    public static double BetaFromInput(ArrayList<String[]> input) {
+        return Double.parseDouble(input.get(0)[3]);
+    }
+
+    public static ArrayList<List<Double>> heightmapFromInput(ArrayList<String[]> input, int I, int J) {
+        ArrayList<List<Double>> ret = new ArrayList<>();
+        for(int i = 0; i < I; i++) {
+            List<Double> row = new ArrayList<>();
+            for(int j = 0; j < J; j++) {
+                row.add(Double.parseDouble(input.get(i+1)[j]));
+            }
+            ret.add(row);
+        }
+        return ret;
     }
 }
